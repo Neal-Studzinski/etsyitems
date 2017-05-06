2f70222994ac3ccb62f0f511270edf74e4f8b740
NEAL STUDZINSKI <nealstudzinski@gmail.com>
2017-05-06 14:26:57 -0500

    working order now

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var ansOutput1 = document.querySelector('#answer1');
-var ansOutput2 = document.querySelector('#answer2');
-var ansOutput3 = document.querySelector('#answer3');
-var ansOutput4 = document.querySelector('#answer4');
-var ansOutput5 = document.querySelector('#answer5');
-var ansOutput6 = document.querySelector('#answer6');
+var ansInput1 = document.querySelector('#answer-1');
+var ansInput2 = document.querySelector('#answer-2');
+var ansInput3 = document.querySelector('#answer-3');
+var ansInput4 = document.querySelector('#answer-4');
+var ansInput5 = document.querySelector('#answer-5');
+var ansInput6 = document.querySelector('#answer-6');
 
 var avg = 0;
 var sum = 0;
@@ -13,7 +13,7 @@ var length = items.length;
 items.forEach(function(currentItem,index,arr) {
 sum += currentItem.price; // takes the price property from the currentItem
 });  // keeps a running total called sum.
-ansOutput1.innerHTML = "The average price is $" + (sum/length).toFixed(2);
+ansInput1.innerHTML = "The average price is $" + (sum/length).toFixed(2);
 // Puts the answer output into the html Doc   toFixed rounds the avg price to 0.00
 
 // #2
@@ -22,7 +22,7 @@ var filteredItemList = items.filter(function(currentItem, index, array){
 } );
 
 filteredItemList.forEach(function(item, index, arr){
- ansOutput2.innerHTML += '<li>' + item.title + '</li>';
+ ansInput2.innerHTML += '<li>' + item.title + '</li>';
 })
 
 
@@ -33,7 +33,7 @@ var filteredCurrency = items.filter(function(currentItem, index, array) {
 })
 
 filteredCurrency.forEach(function(item, index, arr) {
-ansOutput3.innerHTML = item.title + " costs " +  '£' + item.price;
+ansInput3.innerHTML = item.title + " costs " +  '£' + item.price;
 })
 
 
@@ -50,14 +50,41 @@ var hasWood = isWood.map(function(item, index, array) {
 
 hasWood.forEach(function(item, index, array) {
  ansInput3 = document.querySelector('ul#fourl');
- ansOutput4.innerHTML += item;
+ ansInput4.innerHTML += item;
 });
 
-// #5
-var isHomemade = items.filter(function(currentItem, index, array) {
-  return currentItem.who_made.some(function(currentItem, index, array) {
+// ------------------------- Question 5
+// Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+// var eightOrMore = items.filter(function(currentItem, index, array){
+//   return currentItem.materials.length >= 8;
+// });
+//map out materials first
+//use reduce to put materials into an empty string
 
-    return currentItem ===
-  }
+var eightOrMore = items.filter(function(currentItem, index, array){
+  return currentItem.materials.length >= 8;
+});
+
+var eightOrMoreMat = eightOrMore.map(function(item, index, array){
+ return item.materials;
+});
+
+
+
+eightOrMore.forEach(function(currentItem, index, array){
+
+
+  ansInput5.innerHTML += '<li>'+ currentItem.title  + " has " + currentItem.materials.length + " materials.</li>";
+  ansInput5.innerHTML += '<li>'+ currentItem.materials +'</li>'
+
+});
 
 // #6
+
+//how many items were made by their sellers?
+//  18 were made by their sellers
+// "who_made": "i_did",
+var madeBySellers = items.filter(function(currentItem, index, array){
+  return currentItem.who_made === "i_did";
+})
+ansInput6.innerHTML = madeBySellers.length + " were made by their sellers"
